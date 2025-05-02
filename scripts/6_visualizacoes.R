ba2f69d2b0790455caffb907762aa2f805c59e9e
CleideCarnicer <cleidecarnicerbio@gmail.com>
2025-05-02 17:52:43 -0300

    acrescentei gráficos

--- scripts/6_visualizacoes.R
+++ scripts/6_visualizacoes.R
@@ -49,3 +49,75 @@ ggplot(resultados, aes(x = tempo_pri_mov)) +
     x = 'Dias desde a soltura',
     y = ''
   )
+
+
+# Proporcao
+total_ind <- 22
+resultados$numero_ind_prim_mov_jus_prop <- resultados$numero_ind_prim_mov_jus / total_ind
+resultados$numero_ind_prim_mov_mont_prop <- resultados$numero_ind_prim_mov_mont / total_ind
+
+library(scales)
+
+d <- ggplot(resultados, aes(x = tempo_pri_mov)) +
+  geom_line(aes(x = tempo_pri_mov_jus, y = numero_ind_prim_mov_jus_prop, col = "Jusante"), lwd = 1.1) +
+  geom_line(aes(x = tempo_pri_mov_mont, y = numero_ind_prim_mov_mont_prop, col = "Montante"), lwd = 1.1) +
+  scale_color_manual('', values = c("Jusante" = cor_jus, "Montante" = cor_mon)) +
+  scale_x_continuous(n.breaks = 14) +
+  scale_y_continuous(labels = percent_format(accuracy = 1)) +
+  theme_classic(base_size = 14) +
+  theme(
+    legend.position = 'top',
+    legend.text = element_text(size = 14),
+    axis.line.y = element_blank(),
+    plot.margin = margin(20, 10, 4, 4)
+  ) +
+  labs(
+    x = 'Dias desde a soltura',
+    y = 'Proporção acumulada de indivíduos (%)'
+  )
+
+ggsave('img/graficos/movimentos_dias.png', plot = d, width = 8, height = 5)
+
+
+# deslocamento
+dados_longos <- resultados %>%
+  dplyr::select(radio_id, taxa_desloc_media_kmrd, jusante_max, montante_max) %>%
+  pivot_longer(
+    cols = c(taxa_desloc_media_kmrd, jusante_max, montante_max),
+    names_to = "tipo",
+    values_to = "valor") %>%
+  dplyr::mutate(radio_id = factor(radio_id, levels = c("5003", sort(unique(radio_id[radio_id != "5003"])))))
+
+# Gráfico com facetas
+f <- ggplot(dados_longos, aes(x = factor(radio_id), y = valor, fill = tipo)) +
+  geom_col() +
+  facet_wrap(~ tipo, scales = "free_y", labeller = as_labeller(c(
+    "taxa_desloc_media_kmrd" = "Média (kmr/dia)",
+    "jusante_max" = "Máximo a jusante (kmr)",
+    "montante_max" = "Máximo a montante (kmr)"
+  ))) +
+  scale_fill_manual(
+    values = c(
+      "taxa_desloc_media_kmrd" = "#1B9E77",
+      "jusante_max" = '#5b1e00',
+      "montante_max" = '#03405a'
+    ),
+    guide = "none"  # Remove a legenda (já está separada por facetas)
+  ) +
+  labs(
+    x = "Radio ID",
+    y = "Deslocamento (kmr)",
+    # title = "Deslocamento máximo e taxa média por indivíduo"
+  ) +
+  theme_classic(base_size = 11) +
+  theme(
+    strip.text = element_text(size = 11),
+    strip.background = element_blank(),
+    plot.title = element_text(size = 12, hjust = 0.5),
+    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
+    axis.line.y = element_blank(),
+    plot.margin = margin(20, 10, 4, 4)
+  );f
+
+ggsave('img/graficos/deslocamento_medio.png', plot = f, width = 8, height = 5)
+
